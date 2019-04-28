1ed672561269ef2e6adad24a4714e6f331eeafd0
Diordna <yoon91.dev@gmail.com>
2019-04-28 23:38:57 +0900

    Fix description in sorts

--- sort/sort_bubble.py
+++ sort/sort_bubble.py
@@ -1,20 +1,24 @@
 # coding=utf-8
 
-## 버블정렬
-## TODO : 앞의 원소와 뒤의 원소를 비교하여 큰 원소를 뒤로 보내 정렬한다.
+"""
+[버블정렬]
+앞의 원소와 뒤의 원소를 비교하여 큰 원소를 뒤로 보내 정렬한다.
+"""
+
 
 def get_array():
     return [4213, 234, 324, 123, 12, 5, 345, 346, 34, 2354, 243, 34]
+
 
 array = get_array()
 size = len(array)
 
 for x in range(size):
     for i in range(size - 1):
-            before = array[i]
-            after = array[i + 1]
-            if before > after:
-                array[i] = after
-                array[i + 1] = before
+        before = array[i]
+        after = array[i + 1]
+        if before > after:
+            array[i] = after
+            array[i + 1] = before
 
 print array
