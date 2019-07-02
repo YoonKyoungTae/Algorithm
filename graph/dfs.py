7352ea9d659a8b026136735ac6eb8010ae595690
Diordna <yoon91.dev@gmail.com>
2019-07-03 00:42:09 +0900

    Add DFS, BFS

---
+++ graph/dfs.py
@@ -0,0 +1,31 @@
+def dfs(graph, start_node):
+    visit = list()
+    stack = list()
+
+    stack.append(start_node)
+
+    while stack:
+        node = stack.pop()
+        if node not in visit:
+            visit.append(node)
+            stack.extend(graph[node])
+
+    return visit
+
+graph_node = {
+    'A': ['B'],
+    'B': ['A', 'C', 'H'],
+    'C': ['B', 'D', 'G'],
+    'D': ['C', 'E'],
+    'E': ['D', 'F'],
+    'F': ['E'],
+    'G': ['C'],
+    'H': ['B', 'I', 'J', 'M'],
+    'I': ['H'],
+    'J': ['H', 'K'],
+    'K': ['J', 'L'],
+    'L': ['K'],
+    'M': ['H']
+}
+
+print(dfs(graph_node, 'A'))
